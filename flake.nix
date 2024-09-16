077b0210f59c51265ed5759196e2f6af4192bc18
Idris Raja <idris.raja@gmail.com>
2024-09-16 16:53:32 -0500

    easily add tech talk

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/23.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/24.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { nixpkgs, flake-utils, ... }:
     let
@@ -14,8 +14,11 @@
         in with packages; {
           books = callPackage (f "books" "/home/hippoid/books") h;
           papers = callPackage (f "papers" "/home/hippoid/documents/papers") h;
+          techtalk =
+            callPackage (f "techtalk" "/home/hippoid/documents/tech-talks") h;
           booksDesktopItem = callPackage (g "books") { };
           papersDesktopItem = callPackage (g "papers") { };
+          techtalkDesktopItem = callPackage (g "techtalk") { };
         };
     in {
       overlays.all = _: prev: packageSet prev;
